6953fe3c72ec8ab0547d3354f9f1cc4db6aa1e25
iSemicolon <palash.samanta7497@gmail.com>
2024-05-11 15:37:36 +0530

    "Eureka naming server added"

--- currency-conversion-service/src/main/java/org/kol/microservices/currencyconversionservice/feign/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/org/kol/microservices/currencyconversionservice/feign/CurrencyExchangeProxy.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "currency-exchange", url = "localhost:8000")
+//problem here if 8000 port down ,although 8001, 8002 works, we have to hardcoded url for that namin server came into pic
 public interface CurrencyExchangeProxy {
 
 
